e93410217343473e288c8ecdbf12ddbc88bf35be
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-23 11:16:42 -0500

    making skeleton

--- src/react/js/components/App.jsx
+++ src/react/js/components/App.jsx
@@ -1,9 +1,31 @@
 import Banner from "./Banner.jsx"
 
+
+// TODO put the div containers into their own components
 const App = () => {
     return (
         <>
-            <Banner />
+            <div className="body ctn">
+                <div className="nav ctn">
+                    <h1>nav</h1>
+                    {/* <Nav/> */}
+                </div>
+                <div className="banner ctn">
+                    <Banner />
+                </div>
+                <div className="hero ctn">
+                    <h1>hero</h1>
+                </div>
+                {/* <Product Hero */}
+                <div className='main-carousel ctn'>
+                    <h1>main-carousel</h1>
+                </div>
+                <div className='product-description ctn'>
+                    <h1>product-description</h1>
+                </div>
+                
+            </div>
+
         </>
     )
 }
