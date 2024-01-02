97277f9974ecee32a103f3284d0d86df01c42874
Thiem Huynh <thiem.huynh0@gmail.com>
2024-01-02 13:16:48 -0500

    moved navbar to it's own component

---
+++ src/react/js/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+
+    return (
+    <>
+        <div className="nav ctn">
+            <h1>nav</h1>
+        </div>
+        <div className="charity-nav ctn">
+            <h1>charity-nav</h1>
+        </div>
+    </>
+
+    )
+}
+
+export default Navbar
